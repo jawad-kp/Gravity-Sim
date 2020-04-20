4e88d990fb9d3a3418941ac4d27272deefc49aac
studboii <studboii@users.noreply.github.com>
2020-04-20 08:42:23 +0530

    Menu Works and redirects as intended. Added More Display States. Added Comments explaining how then enum is to be interpreted. b lets you go to menu from anywhere q lets you quit from anywhere

--- Gravity-Sim/MotionCalc.cpp
+++ Gravity-Sim/MotionCalc.cpp
@@ -30,13 +30,38 @@ void DrawString(float x, float y, float z, const char* string)
 
 enum class AppStat
 {
-	UNKNOWN = -1, START_SCREEN = 0, MENU = 1  
+	UNKNOWN = -1, START_SCREEN = 0, MENU = 1, PRJMTN_INP_FIRSTVAR = 2, PRJMTN_INP_SECONDVAR = 3, 
+	PRJMTN_DISP = 4, DROP_INP_HT = 5, DROP_DISP = 6, ABOUT_PAGE = 7
+
 };
+//AppStat is an enum that we can use to tell our display mode to switch to a particular mode. 
+/* Unknown we can use to do Error Handling
+	START_SCREEN tells it to display the first initial start screen, MENU is similar
+	PRJMTN = Projectile Motion
+	INP = input mode
+	the next part tells us what we're taking in input for. I'll rename it optimise once we have a calc function that tells us what inputs it needs and compute the equation for it.
+	DISP = Show the animation
+	DROP = Dropping an object
+	ABOUT_PAGE = redirect to about page
+	*/
+
+
 
 AppStat DispStat = AppStat::UNKNOWN;
 
 void KeyProc(unsigned char key, int x, int y)//This is function bound to the keystroke from my keyboard
 {
+	
+	if (key == 'q')//for debug, remove in deploy
+	{
+		exit(0);
+
+	}
+	if (key == 'b')//Remove on deploy Use this to go back to menu incase you need to test something.
+	{
+		DispStat = AppStat::MENU;
+		glutPostRedisplay();
+	}
 	if (DispStat == AppStat::START_SCREEN)
 	{
 		if (key == 'X' || key == 'x')
@@ -49,10 +74,28 @@ void KeyProc(unsigned char key, int x, int y)//This is function bound to the key
 			exit(0);//user go bye byeeeee, khuda haifiz userji
 		}
 	}
-	else if (DispStat == AppStat::MENU && (key == 'q'|| key == 'Q'))
+	else if (DispStat == AppStat::MENU)
 	{
-		exit(0);
-	}//this gonna change when I build the menu out tomorrow and try and do the input boxes too.
+		if (key == '1')
+		{
+			DispStat = AppStat::PRJMTN_INP_FIRSTVAR;
+			glutPostRedisplay();
+		}
+		else if (key == '2')
+		{
+			DispStat = AppStat::DROP_INP_HT;
+			glutPostRedisplay();
+		}
+		else if (key == '3')
+		{
+			DispStat = AppStat::ABOUT_PAGE;
+			glutPostRedisplay();
+		}
+		else if (key == '4' || key == 'q' || key == 'Q')
+		{
+			exit(0);
+		}
+	}
 }
 std::stringstream inp;
 
@@ -72,25 +115,63 @@ void disp()
 
 		DrawString(-0.3, 0.0, 0.0, "Press X to Begin");
 		DrawString(-0.3, -0.1, 0.0, "Press Q to Quit");
-		std::cout << (int)DispStat << std::endl;
+		//std::cout << (int)DispStat << std::endl;
 
 		glFlush();
 	}
 
 	else if (DispStat == AppStat::MENU)
 	{
-		glClearColor(0.7, 0.7, 0.15, 1.0);
+		glClearColor(0.15, 0.15, 0.15, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glLoadIdentity();
 
 		SetFont(GLUT_BITMAP_HELVETICA_18);
 		glColor3f(0.0, 0.0, 0.0);
-		DrawString(-0.3, 0.8, 0.0, "This is the Second Screen. Menu will be dispalyed here");
-		DrawString(-0.3, -0.1, 0.0, "Press Q to Quit");
-		std::cout << (int)DispStat << std::endl;
+		DrawString(-0.3, 0.8, 0.0, "Menu");
+		DrawString(-0.3, 0.6, 0.0, "1) Projectile Simulation");
+		DrawString(-0.3, 0.5, 0.0, "2) Dropping an Object");
+		DrawString(-0.3, 0.4, 0.0, "3) About");
+		DrawString(-0.3, 0.3, 0.0, "4) Exit (You can press Q to leave)");
+		DrawString(-0.3, 0.1, 0.0, "Type in 1,2,3 or 4 Depending on your choice:");
+		//std::cout << (int)DispStat << std::endl;
 		glFlush();
 	}
-	//glutSwapBuffers();
+
+	else if (DispStat == AppStat::PRJMTN_INP_FIRSTVAR || DispStat == AppStat::PRJMTN_INP_SECONDVAR)
+	{
+		glClearColor(0.15, 0.15, 0.15, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+
+		SetFont(GLUT_BITMAP_HELVETICA_18);
+		glColor3f(0.0, 0.0, 0.0);
+		DrawString(-0.3, 0.8, 0.0, "Projectile Screen");
+		glFlush();
+	}
+	else if (DispStat == AppStat::DROP_INP_HT)
+	{
+		glClearColor(0.15, 0.15, 0.15, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+
+		SetFont(GLUT_BITMAP_HELVETICA_18);
+		glColor3f(0.0, 0.0, 0.0);
+		DrawString(-0.3, 0.8, 0.0, "Drop Screen");
+		glFlush();
+	}
+	else if (DispStat == AppStat::ABOUT_PAGE)
+	{
+		glClearColor(0.15, 0.15, 0.15, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+
+		SetFont(GLUT_BITMAP_HELVETICA_18);
+		glColor3f(0.0, 0.0, 0.0);
+		DrawString(-0.3, 0.8, 0.0, "About:");
+		glFlush();
+	}
+	glutSwapBuffers();
 
 	
 }
